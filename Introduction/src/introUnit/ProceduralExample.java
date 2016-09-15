108c867b71c25366038235aeb60ccf30c3630334
Mothballs017 <mchu1537@bths.edu>
2016-09-15 12:59:24 -0400

    Wrote Hello World program

--- Introduction/src/introUnit/ProceduralExample.java
+++ Introduction/src/introUnit/ProceduralExample.java
@@ -8,5 +8,8 @@ package introUnit;
  *
  */
 public class ProceduralExample {
-
+  public static void main (String[] args){
+    System.out.println("Hello World");
+    //class.field.method("message");
+  }
 }
