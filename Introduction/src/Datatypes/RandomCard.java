e1f4db1125bfea5b48d6e84875202f6edaa06f48
ecuck <ecuck@Batcomputer.fios-router.home>
2016-09-21 23:22:13 -0400

    deleted extra value...stupid

--- Introduction/src/Datatypes/RandomCard.java
+++ Introduction/src/Datatypes/RandomCard.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class RandomCard {
 
 	public static void main(String[] args) {
-		String[] value = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+		String[] value = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
 		String[] suite = {"Diamonds", "Clubs", "Hearts", "Spades"};
 		
 		int card = drawCard();
@@ -17,13 +17,13 @@ public class RandomCard {
 	
 	public static int drawCard(){
 		double rand = Math.random();
-		int roll = (int)(13 * rand);
+		int roll = (int)(12 * rand);
 		return roll + 1;
 	}
 	
 	public static int selectSuite(){
 		Random gen = new Random();
-		return gen.nextInt(4) + 1;
+		return gen.nextInt(3) + 1;
 	}
 
 }
