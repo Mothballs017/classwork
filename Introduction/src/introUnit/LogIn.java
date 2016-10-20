382eb4afb72a5638e422e7b8a7b57556db466e24
Student 6 <Student 6@10.8.33.165>
2016-10-20 12:19:05 -0400

    Login test possible answer

---
+++ Introduction/src/introUnit/LogIn.java
@@ -0,0 +1,61 @@
+package introUnit;
+
+import java.util.Scanner;
+
+public class LogIn {
+
+	static Scanner input = new Scanner(System.in);
+
+	public static String waitForEntry(){
+		return input.nextLine();
+	}
+
+	//1 point for "private" or inaccessible
+	//1 point for declaration of data type (String)
+	private static String username = "test_user";
+	private static String password = "test";
+
+	//1 point correct method header
+	public static void main(String[] args){
+		if(correctUser()){
+			askPassword();
+		}
+		else{
+			System.out.println("Unknown username. Please contact network administrator");
+		}
+	}
+
+	private static void askPassword() {
+		boolean inLoop = true;
+		int remainingTries = 3;
+		while(inLoop){
+			System.out.println("Enter password.");
+			String entry = waitForEntry();
+			if(entry.equals(password)){
+				System.out.println("You are in!");
+				inLoop = false;
+			}
+			else{
+				remainingTries--;
+				if(remainingTries==0){
+					System.out.println("Invalid password");
+					inLoop = false;
+				}
+				else{
+					System.out.println("Incorrect password. You have "+remainingTries+" left.");
+				}
+			}
+		}
+	}
+
+	private static boolean correctUser() {
+		//System.out.println("Enter username.");
+		////waitForEntry.equals for Strings
+		//if(waitForEntry().equals(username)){
+		//	return true;
+		//}
+		//return false;
+		System.out.println("Enter username.");
+		return waitForEntry().equals(username);
+	}
+}
