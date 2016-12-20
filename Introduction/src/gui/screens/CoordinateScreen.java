de7255141f1a164f223b0c74e2d21025a8801757
Student 6 <Student 6@10.8.33.165>
2016-12-20 13:18:46 -0500

    aed

--- Introduction/src/gui/screens/CoordinateScreen.java
+++ Introduction/src/gui/screens/CoordinateScreen.java
@@ -35,7 +35,7 @@ public class CoordinateScreen extends Screen implements MouseMotionListener{
 			}
 		});
 		area = new TextArea(20, 300, 700, 100, "ajksf asdf sdf sdf sdf scv sdg gv sd wef wsf sc sf sf sdg s sg sdf df sd fs df sdg d");
-		bat = new Graphic(150,30,100,100,"resources/sampleImages/bat.jpg");
+		bat = new Graphic(150,30,.5,"resources/sampleImages/bat.jpg");
 		viewObjects.add(text);
 		viewObjects.add(button);
 		viewObjects.add(area);
